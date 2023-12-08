8f2f9f2156ece70c83f8d30b8dba58eeb2951a42
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-12-09 07:12:26 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp17/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace ConsoleApp17;
+
+public class Plugin
+{
+    private readonly HttpClient _client = new();
+
+    [KernelFunction, Description("今日の日付を返す")]
+    public static string Today()
+    {
+        return DateTime.Now.ToString("yyyy/MM/dd");
+    }
+
+    [KernelFunction, Description("今日の曜日を返す")]
+    public static string DayOfWeek()
+    {
+        return DateTime.Now.ToString("dddd");
+    }
+
+    [KernelFunction, Description("今の時間を返す")]
+    public static string Now()
+    {
+        return DateTime.Now.ToString("HH:mm:ss");
+    }
+
+    [KernelFunction, Description("東京の天気を返す")]
+    public async Task<string> Weather()
+    {
+        return (await _client.GetAsync("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json")).Content.ReadAsStringAsync().Result;
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp17;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .Build();
+
+string deploymentName = config["OpenAI:DeploymentName"] ?? throw new InvalidOperationException("OpenAI:DeploymentName is not set.");
+string modelId = config["OpenAI:ModelId"] ?? throw new InvalidOperationException("OpenAI:ModelId is not set.");
+string endpoint = config["OpenAI:Endpoint"] ?? throw new InvalidOperationException("OpenAI:BaseUrl is not set.");
+string key = config["OpenAI:Key"] ?? throw new InvalidOperationException("OpenAI:Key is not set.");
+
+KernelBuilder builder = new();
+
+
+builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName,
+    modelId,
+    endpoint, 
+    key);
+builder.Plugins.AddFromType<Plugin>();
+
+Kernel kernel = builder.Build();
+OpenAIPromptExecutionSettings? setting = new()
+{
+    FunctionCallBehavior = FunctionCallBehavior.AutoInvokeKernelFunctions
+};
+
+while (true)
+{
+    Console.Write("User > ");
+    string input = Console.ReadLine()!;
+    if (input == "exit")
+    {
+        break;
+    }
+    else
+    {
+        var result = await kernel.InvokePromptAsync(input, new(setting));
+        Console.WriteLine($"Assistant > {result}");
+    }
+}
